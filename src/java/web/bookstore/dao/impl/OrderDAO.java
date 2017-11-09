0206587a6080e79896c750f25d66f78dbe990606
Lucas Santos <lucas.santos.silva2011@gmail.com>
2017-11-08 23:51:36 -0200

    order: added unimplemented methods

--- src/java/web/bookstore/dao/impl/OrderDAO.java
+++ src/java/web/bookstore/dao/impl/OrderDAO.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package web.bookstore.dao.impl;
 
-/**
- *
- * @author lucas
- */
-public class OrderDAO {
+import java.util.List;
+import web.bookstore.domain.DomainEntity;
+
+public class OrderDAO extends AbstractDAO {
+
+    @Override
+    public DomainEntity save(DomainEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public DomainEntity update(DomainEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public DomainEntity select(DomainEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean delete(DomainEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<DomainEntity> list(DomainEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<DomainEntity> search(DomainEntity entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
